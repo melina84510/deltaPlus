31cb90ba057d1744ec2b34bc7421ebe6c651ef92
melina84510 <melina.meneghini@hotmail.fr>
2025-02-27 14:27:43 +0100

    test de git

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 
 <title>Delta Plus | Entreprise Familiale Leader en Fabrication d'EPI</title>
 <meta name="description" content="Découvrez Delta Plus, une entreprise familiale et leader dans la fabrication d'Équipements de Protection Individuelle (EPI). Forts de notre héritage, nous protégeons les travailleurs du monde entier avec des solutions innovantes et fiables.">
+<!-- Test de git -->
 
 <?php include('header.php'); ?>
     <main>
